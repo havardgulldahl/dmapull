9ebb6113599f26faa47217b01db5463b410d26a5
Håvard Gulldahl <havard@gulldahl.no>
2015-04-26 01:53:51 +0200

    REFACTOR playing in background.js, add noop id3 reader

--- dmapull.js
+++ dmapull.js
@@ -3,40 +3,132 @@
 
 console.log("dmapull.js loaded");
 
-//var Q = []; // a global queue of intercepted mp3 urls
+function metadata(mp3url) { // get id3 data via id3.js
+    var tok = mp3url + "_id3";
+    var incache = localStorage.getItem(tok) || false;
+    if(incache) {return incache; }
+    return {}; // bypass until IIS Range bug is fixed
+    
+    ID3.loadTags(mp3url, function() {
+        var tags = ID3.getAllTags(mp3url);
+        console.log(tags);
+        localStorage.setItem(tok, tags);
+        return tags;
+    });
+    return {};
+}    
 
-var windowopened = false;
+
+var Q = {  // a global queue of intercepted mp3 urls
+    init : function() {
+        Q._q = JSON.parse( localStorage.getItem("dmapullQ")) || []; // array of url strings
+        Q._niceq = []; // array of objects with id3 tags and url
+        return Q._q;
+    },
+    add : function(url) {
+        Q._q.push(url);
+        var md = {"url":url, "metadata": metadata(url)};
+        Q._niceq.push(md);
+        chrome.runtime.sendMessage({"msg":"new", "item":md});
+        return Q.length();
+    },
+    save : function() {
+        return localStorage.setItem("dmapullQ", JSON.stringify(Q._q));
+    },
+    length : function() {
+        return Q._q.length;
+    },
+    toArray : function() {
+        return Q._q;
+    },
+    toNiceArray : function() {
+        return Q._niceq;
+    },
+    get : function(idx) {
+        return Q._q[idx];
+    }
+
+};
+Q.init(); // pull from persistant storage on startup
+
+var player = {
+    hardware: jQuery("<audio/>").attr({preload: "auto", autobuffer: true}),
+    play : function(url) {
+        console.log("hardware %o: ", url);
+        if(url !== undefined) {
+            player.hardware.attr({"src": url});
+        }
+        player.hardware.trigger("play");
+        chrome.runtime.sendMessage({"msg":"playerstate", "item":"playing"});
+    },
+    pause : function() {
+        player.hardware.trigger("pause");
+        chrome.runtime.sendMessage({"msg":"playerstate", "item":"paused"});
+    },
+    status : function() {
+        var status, a = player.hardware[0];
+        if(a.paused) { status = "paused"; }
+        else if(a.ended) { status = "stopped"; }
+        else if(a.duration > 0 && !a.paused) { status = "playing"; }
+        else  { status = "loading"; }
+        chrome.runtime.sendMessage({"msg":"playerstate", "item":status});
+        return status;
+    }    
+}
+
+
+    
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      switch(request.msg) {
+            case "rawQ":
+                sendResponse(Q.toArray());
+                break;
+            case "Q":
+                sendResponse(Q.toNiceArray());
+                break;
+            case "playerstate":
+                sendResponse(player.status());
+                break;
+            case "metadata":
+                sendResponse(metadata(request.item));
+                break;
+            case "play":
+                player.play(request.item);
+                break;
+            case "pause":
+                player.pause();
+                break;
+            case "download":
+                chrome.downloads.download({url:request.item});
+                break;
+
+          
+      }
+});
+
 
 function popopQ(mp3item, tab_id) {
-	//Q.push(mp3item);
-    if(windowopened) {
-        chrome.windows.create({url:"popup.html", tabId: tab_id, type: "popup", focused: false }, 
-            function(opened)  { windowopened = opened.id; }
-        );
-        
-    }
-    chrome.runtime.sendMessage({"msg":"newurl", "url":mp3item}, function(currentQ) {
-        console.log("got reply: %o", currentQ);
-        chrome.pageAction.show(tab_id);
-        chrome.pageAction.setPopup({popup: "popup.html", tabId:tab_id});
-        chrome.pageAction.setIcon({path: "favicon.ico", tabId: tab_id});
-        if(currentQ === undefined) { return; }
-        chrome.pageAction.setBadgeText({text: currentQ.length.toString(), tabId:tab_id});
-        chrome.pageAction.setTitle({title: currentQ.length.toString() + " tracks in queue", tabId:tab_id});
+	Q.add(mp3item);
+    console.log("play %o already (%o)", mp3item, player.status());
+    if(["paused", "stopped"].indexOf(player.status()) > -1) {  player.play(mp3item); }
+    chrome.pageAction.show(tab_id);
+    chrome.pageAction.setPopup({popup: "popup.html", tabId:tab_id});
+    chrome.pageAction.setIcon({path: "favicon.ico", tabId: tab_id});
+    chrome.pageAction.setTitle({title: Q.length().toString() + " tracks in queue", tabId:tab_id});
 
-    });
 } 
 //chrome.webRequest.onCompleted.addListener(
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    console.log("dma intercepted: %o", info);
+    //console.log("dma intercepted: %o", info);
     var cancel = {cancel: true};
 	if(info.url.indexOf("9876543210fedcba") != -1) {
-		// let this through
+		// let this through, it's our own call
 		return {};
 	}
-	jQuery.get(info.url + "?9876543210fedcba", function(data) {
+	jQuery.get(info.url + "?9876543210fedcba", function(data) { // get the playlist, and add our own identifier
 		//console.log("got data: %o", data);
 		var myRegexp = /(http.*\.mp3)/g	;
 		var match;
--- popup.js
+++ popup.js
@@ -1,72 +1,49 @@
 // popup.js
 /*jslint browser: true, devel: true, nomen: true, sloppy: true, stupid: true, white: true */
 
-var Q = {  // a global queue of intercepted mp3 urls
-    pull : function() {
-        Q._q = JSON.parse( localStorage.getItem("dmapullQ")) || [];
-        return Q._q;
-    },
-    add : function(url) {
-        Q._q.push(url);
-        return Q.length();
-    },
-    save : function() {
-        return localStorage.setItem("dmapullQ", JSON.stringify(Q._q));
-    },
-    length : function() {
-        return Q._q.length;
-    },
-    toArray : function() {
-        return Q._q;
-    },
-    get : function(idx) {
-        return Q._q[idx];
-    }
-
-};
-Q.pull();
-
-function play(id) {
-    var url = Q.get(id);
-    jQuery("#player").attr("src", url).trigger("play");
-    
-}
-
-function download(id) {
-    var u = Q.get(id);
-    chrome.downloads.download({url:u});
-    
-}
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log("got msg: %o", message);
-    if(message.msg == "newurl") {
-        console.log("new mp3 url: %o", message.url);
-        Q.add(message.url);
-        jQuery("#title").html(Q.length() + "tracks");
-        createQhtml();
-        sendResponse(Q.toArray());
-        return true;
-    }
-    sendResponse(null);
-    return true;
-    
-});
-
+var Q, playerstate;
 
 function createQhtml() {
     var e = jQuery("#list").empty();
-    for(var i=0; i<Q.length();i++) {
-        jQuery("<li data-id="+i+"><button data-cmd=play>play</button><button data-cmd=download>get</button></li>")
+    for(var i=0; i<Q.length;i++) { 
+        var t = Q[i].metadata.title || "Song #"+i;
+        jQuery("<li data-id="+i+">"+t+"<button data-cmd=play>play</button><button data-cmd=download>get</button></li>")
             .appendTo(e);
     }
     e.on("click", function(ev) {
-        console.log("click: %o", ev);
+        //console.log("click: %o", ev);
         var btn = jQuery(ev.target);
         var _id = btn.parent().data("id");
         console.log("id: %o", _id);
-        if(btn.data("cmd") == "play") { play(_id); }
-        else { download(_id); }
+        switch(btn.data("cmd")) {
+
+            case "play": 
+                chrome.runtime.sendMessage({"msg":"play", "item":Q[_id].url}); break;
+            case "pause": 
+                chrome.runtime.sendMessage({"msg":"pause", "item":Q[_id].url}); break;
+            case "download": 
+                chrome.runtime.sendMessage({"msg":"download", "item":Q[_id].url}); break;
+        }
     });
     
 }
+
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      switch(request.msg) {
+        case "playerstate":
+            playerstate = request.item;
+            break;
+      }
+    }
+);
+
+// get current queue on popup
+jQuery(document).ready(function() {
+    console.log("popup");
+    chrome.runtime.sendMessage({msg:"Q"}, function(response) {
+        console.log("got Q %o", response);
+        Q = response;
+        createQhtml();
+    });
+});
