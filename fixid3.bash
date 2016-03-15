abdb92b4d25cee6aa24920682aef2d3658ce9ca9
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2016-03-15 14:41:47 +0100

    bettr parsing

--- fixid3.bash
+++ fixid3.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for z in $*;
+for z in "$@";
 do 
-    id3v2 -C $z; 
-    TITLE=`id3v2 -l $i | iconv -f latin1 | grep TIT2 | cut -c44- | sed -e 's/^[[:space:]]*//'`; echo --$TITLE-$i--; 
-    mv -iv $i "$TITLE";
+    id3v2 -C "$z"; 
+    TITLE=`id3v2 -l "$z" | iconv -f latin1 | grep TIT2 | cut -c44- | sed -e 's/^[[:space:]]*//'`; echo --$TITLE--; 
+    mv -iv "$z" "$TITLE-$z";
 done
